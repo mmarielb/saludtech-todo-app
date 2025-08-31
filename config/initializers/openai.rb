356d2873463fda880e74990a6d33ac960e682756
marielb2512 <mmarquez@buk.cl>
2025-08-31 17:24:24 -0400

    fix: se corrige error para render

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,5 +1,7 @@
-require 'openai'
-
-OpenAIClient = OpenAI::Client.new(
-  access_token: ENV.fetch('OPENAI_API_KEY')
-)
+if ENV['OPENAI_API_KEY'].present?
+  OpenAI.configure do |config|
+    config.access_token = ENV['OPENAI_API_KEY']
+  end
+else
+  Rails.logger.warn 'OPENAI_API_KEY no está configurada. Se usará la heurística local.'
+end
